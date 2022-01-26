3a0c90fd0989e1a9042deea5eb18f01aebf9a317
minosmlee <52672287+minosmlee@users.noreply.github.com>
2022-01-26 16:38:26 +0900

    1st

---
+++ fileupload/fileupload.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const express = require('express');
+const multer = require('multer');
+const { copyFile } = require('fs/promises');
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+
+var storage = multer.diskStorage({
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+})
+
+var upload = multer({
+    storage: storage
+});
+
+app.get("/", (req, res, next) => {
+    fs.readFile("./index.html", (err, data) => {
+        if(err) throw err;
+        res.write(data);
+        res.end();
+    });
+});
+
+app.post("/file-upload", upload.single('file'), (req, res) => {
+    let file = req.file
+    res.send(`${file.filename} is successfully uploaded. file.size is ${file.size}.`);
+});
+
+app.listen(8080);
